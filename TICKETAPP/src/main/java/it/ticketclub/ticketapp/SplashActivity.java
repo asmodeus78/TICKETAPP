fd717e6c657b7ba5653b703ae406a4e7e8744eff
asmodeus78 <asmodeus78@gmail.com>
2014-03-20 21:23:24 +0100

    -Passaggio ad altro tash funzionante

--- TICKETAPP/src/main/java/it/ticketclub/ticketapp/SplashActivity.java
+++ TICKETAPP/src/main/java/it/ticketclub/ticketapp/SplashActivity.java
@@ -1,12 +1,56 @@
 package it.ticketclub.ticketapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
 
 
 public class SplashActivity extends Activity {
+
+    private static final String TAG_LOG = SplashActivity.class.getName();
+
+    private static final long MIN_WAIT_INTERVAL = 1500L;
+    private static final long MAX_WAIT_INTERVAL = 3000L;
+    private static final int GO_HEAD_WHAT = 1;
+    private long mStartTime;
+    private boolean mIsDone;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case GO_HEAD_WHAT:
+                    long elapsedTime = SystemClock.uptimeMillis() - mStartTime;
+                    if (elapsedTime>=MIN_WAIT_INTERVAL && !mIsDone){
+                        mIsDone = true;
+                        goAhead();
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mStartTime = SystemClock.uptimeMillis();
+        final Message goAheadMessage = mHandler.obtainMessage(GO_HEAD_WHAT);
+        mHandler.sendMessageAtTime(goAheadMessage, mStartTime + MAX_WAIT_INTERVAL);
+        Log.d(TAG_LOG, "Handler message sent!");
+    }
+
+    private void goAhead(){
+        // We create the explicit Intent
+        final Intent intent = new Intent(this,MainActivity.class);
+        // Launch the Intent
+        startActivity(intent);
+        // We finish the current Activity
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +58,5 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.activity_splash);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.splash,menu);
-        return true;
-    }
+
 }
