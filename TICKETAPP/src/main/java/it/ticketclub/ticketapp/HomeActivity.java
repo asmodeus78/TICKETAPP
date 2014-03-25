c97dd83920eda4745e160815ee6dc891055102e5
asmodeus78 <asmodeus78@gmail.com>
2014-03-25 21:42:24 +0100

    -- Sistema senza web view con parser funzionante bug al caricamento immagine

--- TICKETAPP/src/main/java/it/ticketclub/ticketapp/HomeActivity.java
+++ TICKETAPP/src/main/java/it/ticketclub/ticketapp/HomeActivity.java
@@ -1,60 +1,183 @@
 package it.ticketclub.ticketapp;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.client.methods.*;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpEntity;
+import android.os.AsyncTask;
+import android.widget.ListView;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 
+
+
+import android.util.Log;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import org.json.JSONArray;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+
 
 //http://developer.android.com/reference/android/util/JsonReader.html
 //http://stackoverflow.com/questions/9605913/how-to-parse-json-in-android?answertab=active#tab-top
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends ListActivity {
+
+    private ProgressDialog pDialog;
+
+    // URL to get tickets JSON
+    //private static String url = "http://api.androidhive.info/contacts/";
+    private static String url = "http://www.ticketclub.it/APP/gateway2.php?CMD=TK";
+
+    // JSON Node names
+    private static final String TAG_LISTA = "TICKET";
+    private static final String TAG_ID = "idTicket";
+    private static final String TAG_CODICE = "codice";
+    private static final String TAG_OGGETTO = "oggetto";
+    private static final String TAG_TITOLO = "titoloSup";
+    private static final String TAG_IMAGE = "image";
+
+    /*private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+    private static final String TAG_PHONE_OFFICE = "office";*/
+
+    // tickets JSONArray
+    JSONArray tickets = null;
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, Object>> ticketList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        ticketList = new ArrayList<HashMap<String, Object>>();
+
+        //ListView lv = getListView();
+
+        // Calling async task to get json
+        new GetTickets().execute();
+
+    }
 
 
-        DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
-        HttpPost httppost = new HttpPost("http://someJSONUrl/jsonWebService");
-        // Depends on your web service
-        httppost.setHeader("Content-type", "application/json");
 
-        InputStream inputStream = null;
-        String result = null;
-        try {
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
+    private class GetTickets extends AsyncTask<Void, Void, Void> {
 
-            inputStream = entity.getContent();
-            // json is UTF-8 by default
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-            StringBuilder sb = new StringBuilder();
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(HomeActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
 
-            String line = null;
-            while ((line = reader.readLine()) != null)
-            {
-                sb.append(line + "\n");
-            }
-            result = sb.toString();
-        } catch (Exception e) {
-            // Oops
         }
-        finally {
-            try{if(inputStream != null)inputStream.close();}
-            catch(Exception squish){
 
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    tickets = jsonObj.getJSONArray("TICKET");
+
+                    // looping through All Tickets
+                    for (int i = 0; i < tickets.length(); i++) {
+                        JSONObject c = tickets.getJSONObject(i);
+
+                        String id = c.getString(TAG_ID);
+                        String codice = c.getString(TAG_CODICE);
+                        String titolo = c.getString(TAG_TITOLO);
+                        String oggetto = c.getString(TAG_OGGETTO);
+
+                        Drawable imgticket = null;
+
+                        String photo = "http://www.ticketclub.it/TICKET_NEW/biglietti/" + c.getString(TAG_CODICE) + ".jpg";
+
+                        try {
+                            InputStream in = (InputStream) new URL(photo).getContent();
+                            imgticket = Drawable.createFromStream(in,codice);
+                            //InputStream in = new java.net.URL(photo).openStream();
+                            //imgticket = BitmapFactory.decodeStream(in);
+                        } catch (Exception e) {
+                            Log.e("Error", e.getMessage());
+                            e.printStackTrace();
+                        }
+
+
+
+                        // tmp hashmap for single contact
+                        HashMap<String, Object> ticket = new HashMap<String, Object>();
+
+                        // adding each child node to HashMap key => value
+                        ticket.put(TAG_ID, id);
+                        ticket.put(TAG_CODICE, codice);
+                        ticket.put(TAG_TITOLO, titolo);
+                        ticket.put(TAG_OGGETTO, oggetto);
+                        ticket.put(TAG_IMAGE, imgticket);
+
+                        // adding contact to contact list
+                        ticketList.add(ticket);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
             }
+
+            return null;
         }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+
+
+
+             ListAdapter adapter = new SimpleAdapter(
+                    HomeActivity.this, ticketList,
+                    R.layout.list_item, new String[] { TAG_CODICE, TAG_TITOLO,
+                    TAG_OGGETTO, TAG_IMAGE }, new int[] { R.id.name,
+                    R.id.email, R.id.mobile, R.id.image });
+
+
+
+            setListAdapter(adapter);
+
+
+        }
+
     }
 
 
