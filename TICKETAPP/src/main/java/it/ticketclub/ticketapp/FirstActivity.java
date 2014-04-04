3b7d99d60464d5e6b6cbebc195b756b6f89b9563
asmodeus78 <asmodeus78@gmail.com>
2014-04-04 20:59:09 +0200

    -- prova d' implementazione ticket non ancora funzionante

--- TICKETAPP/src/main/java/it/ticketclub/ticketapp/FirstActivity.java
+++ TICKETAPP/src/main/java/it/ticketclub/ticketapp/FirstActivity.java
@@ -1,8 +1,17 @@
 package it.ticketclub.ticketapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,21 +20,30 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class FirstActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+
+
+
 
     private MenuItem mSpinnerItem = null;
 
@@ -53,6 +71,8 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+
 
         //actionBar.setDisplayShowTitleEnabled(false);
         //actionBar.setDisplayShowHomeEnabled (false);
@@ -284,6 +304,37 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
     public static class PlaceholderFragment extends Fragment {
 
 
+
+        /***************************************
+         TEST INSERIMENTO TICKET LOADING
+         ********************************************/
+        private ProgressDialog pDialog;
+
+        // URL to get tickets JSON
+        //private static String url = "http://api.androidhive.info/contacts/";
+        private static String url = "http://www.ticketclub.it/APP/gateway2.php?CMD=TK";
+
+        // JSON Node names
+        private static final String TAG_ID = "idTicket";
+        private static final String TAG_CODICE = "codice";
+        private static final String TAG_OGGETTO = "oggetto";
+        private static final String TAG_TITOLO = "titoloSup";
+        private static final String TAG_IMAGE = "image";
+
+
+        // tickets JSONArray
+        JSONArray tickets = null;
+
+        // Hashmap for ListView
+        ArrayList<HashMap<String, Object>> ticketList;
+
+        /***************************************
+         * fine TEST
+         */
+
+
+
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -314,6 +365,11 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
             // Calling async task to get json
             //new HomeActivity();
 
+            ticketList = new ArrayList<HashMap<String, Object>>();
+
+            // Calling async task to get json
+            new GetTickets().execute();
+
 
 
 
@@ -323,6 +379,129 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
             textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
+
+        private class GetTickets extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                // Showing progress dialog
+                pDialog = new ProgressDialog(getActivity());
+                pDialog.setMessage("Please wait...");
+                pDialog.setCancelable(false);
+                pDialog.show();
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... arg0) {
+                // Creating service handler class instance
+                ServiceHandler sh = new ServiceHandler();
+                BitmapUtils Bu = new BitmapUtils();
+
+                // Making a request to url and getting response
+                String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+                Log.d("Response: ", "> " + jsonStr);
+
+                if (jsonStr != null) {
+                    try {
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+
+                        // Getting JSON Array node
+                        tickets = jsonObj.getJSONArray("TICKET");
+
+                        // looping through All Tickets
+                        for (int i = 0; i < tickets.length(); i++) {
+                            JSONObject c = tickets.getJSONObject(i);
+
+                            String id = c.getString(TAG_ID);
+                            String codice = c.getString(TAG_CODICE);
+                            String titolo = c.getString(TAG_TITOLO);
+                            String oggetto = c.getString(TAG_OGGETTO);
+                            String photo = "http://www.ticketclub.it/TICKET_NEW/biglietti/" + c.getString(TAG_CODICE) + ".jpg";
+                            Drawable d1 = null;
+                            try {
+
+                                InputStream is1 = (InputStream) new URL(photo).getContent();
+                                d1 = Drawable.createFromStream(is1, "src name");
+
+                                //Bitmap imgticket = Bu.loadBitmap(photo);
+
+                            } catch (MalformedURLException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+                            Log.d("COLONNA",photo);
+
+                            // tmp hashmap for single contact
+                            HashMap<String, Object> ticket = new HashMap<String, Object>();
+
+                            // adding each child node to HashMap key => value
+                            ticket.put(TAG_ID, id);
+                            ticket.put(TAG_CODICE, codice);
+                            ticket.put(TAG_TITOLO, titolo);
+                            ticket.put(TAG_OGGETTO, oggetto);
+                            //ticket.put(TAG_IMAGE, Bu.loadBitmap(photo));
+                            ticket.put(TAG_IMAGE, d1);
+
+                            //BitmapFactory.decodeResource(context.)
+
+
+
+
+                            // adding contact to contact list
+                            ticketList.add(ticket);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.e("ServiceHandler", "Couldn't get any data from the url");
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                // Dismiss the progress dialog
+                if (pDialog.isShowing())
+                    pDialog.dismiss();
+                /**
+                 * Updating parsed JSON data into ListView
+                 * */
+
+
+                String[] from = { TAG_CODICE, TAG_TITOLO, TAG_OGGETTO, TAG_IMAGE };
+                int[] views = { R.id.name, R.id.email, R.id.mobile, R.id.image };
+
+                ListAdapter adapter = new SimpleAdapter(
+                        getActivity(),
+                        ticketList,
+                        R.layout.list_item,
+                        from,
+                        views);
+
+               // setListAdapter(adapter);
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+
+    private void loadTicket(){
+
 
 
     }
@@ -336,6 +515,20 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
         if (view instanceof Spinner) {
             Spinner spinner = (Spinner) view;
 
+
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
             ArrayAdapter<CharSequence> listAdapter = ArrayAdapter.createFromResource(context,
                     R.array.spinner_data,
                     R.layout.support_simple_spinner_dropdown_item);
@@ -347,4 +540,9 @@ public class FirstActivity extends ActionBarActivity implements ActionBar.TabLis
     }
 
 
+
+
+
 }
+
+
