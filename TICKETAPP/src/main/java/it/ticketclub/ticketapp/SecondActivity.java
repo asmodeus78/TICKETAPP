ad13bd9bfe0287d27faa295e31f04a431de42e35
Giovanni <asmodeus78@gmail.com>
2014-06-06 21:17:10 +0200

    -- sistemazione pagina offerta -- tasto download logica e grafica -- crediti del ticket -- data scadenza e calcolo tempo rimanente

--- TICKETAPP/src/main/java/it/ticketclub/ticketapp/SecondActivity.java
+++ TICKETAPP/src/main/java/it/ticketclub/ticketapp/SecondActivity.java
@@ -289,6 +289,9 @@ public class SecondActivity extends ActionBarActivity implements ActionBar.TabLi
             final TextView textView = (TextView) rootView.findViewById(R.id.TK_nominativo);
             final TextView textView1 = (TextView) rootView.findViewById(R.id.TK_indirizzo);
             final TextView textView2 = (TextView) rootView.findViewById(R.id.TK_telefono);
+
+
+
             final TextView textView3 = (TextView) rootView.findViewById(R.id.TK_sitoweb);
 
             textView1.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
